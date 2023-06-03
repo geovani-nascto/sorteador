a7a6e558621ffc0c6ff5074e0b48aeb3a0e7046c
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2023-06-02 22:09:34 -0300

    att css finish

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const btnDraw = document.querySelector('.btnDraw');
+const textarea = document.querySelector('textarea');
+const result = document.querySelector('.result p');
+const popUpResult = document.querySelector('.result');
+const btnClose = document.querySelector('.CloseBtn')
+
+function getFormData(){
+
+}
+
+function generateRandonNumber(){
+
+}
+
+function showResult(){
+
+}
+
+function closePopUp(){
+    
+}
