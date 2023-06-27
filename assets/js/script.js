51c706e3078a5a25cfcad925ec9c2861443fab43
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2023-06-26 22:11:41 -0300

    att tab navigations

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,36 @@ const result = document.querySelector('.result--final');
 const popUpResult = document.querySelector('.result');
 //Botão para fechar popup
 const btnClose = document.querySelector('.closeBtn')
+//Tab para mostrar o sorteador de nomes
+const option1 = document.getElementById('option1');
+//Tab para mostrar o sorteador de números
+const option2 = document.getElementById('option2');
+//Sessão com o sorteador de nomes
+const content1 = document.getElementById('content1');
+//Sessão com o sorteador de números
+const content2 = document.getElementById('content2');
+let choose = 1;
 
-function getFormData(event){
+const changeDrawer = () => {
+    option1.className = choose == 1 ? 'option option-active' : 'option';
+    content1.className = choose == 1 ? 'content content-active' : 'content';
+
+    option2.className = choose == 2 ? 'option option-active' : 'option';
+    content2.className = choose == 2 ? 'content content-active' : 'content';
+
+}
+
+option1.addEventListener('click', () => {
+    choose = 1;
+    changeDrawer();
+})
+
+option2.addEventListener('click', () => {
+    choose = 2;
+    changeDrawer();
+})
+
+function getFormData(event) {
     event.preventDefault();
     // Pegar valor digitado no textarea
     const inputValue = textarea.value;
@@ -32,17 +60,17 @@ function getFormData(event){
 btnDraw.addEventListener('click', getFormData);
 
 // O tamanho do array será importante para sortear um valor o utilizando como o máximo
-function generateRandonNumber(arrayLength){
+function generateRandonNumber(arrayLength) {
     // Irá retornar um número inteiro aleatório de acordo com o tamanho do array, sendo o zero o primeiro valor (?)
     return Math.floor(Math.random() * (arrayLength - 0) + 0);
 }
 
-function showResult(name){
+function showResult(name) {
     result.innerText = `Resultado: ${name}`;
     popUpResult.classList.add('showResult');
 }
 
-function closePopUp(){
+function closePopUp() {
     popUpResult.classList.remove('showResult');
 }
 
